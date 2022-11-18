d3f2ad9033368e392ffe2c0c2acb609fda0ef253
chrismulford <chris.mulford@outlook.com>
2022-11-18 11:28:12 +0000

    created sample login credentials

--- auth.py
+++ auth.py
@@ -2,9 +2,8 @@ import json
 
 # TODO: turn into a class and allow url to change
 
-BASE_URL = 'https://kubrick-dev.collibra.com/rest/2.0/'
-
 with open('creds.json', 'r') as f:
     data = json.load(f)
 
 CREDENTIALS = (data['username'], data['password'])
+BASE_URL = data['environment_url']
--- community.py
+++ community.py
@@ -38,3 +38,4 @@ class Community:
 
 if __name__ == '__main__':
     com = Community('Christopher Mulford')
+    print(vars(com))
