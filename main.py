d285883678d00295ebf4eb7cdb46aaf7ae5eb22e
chrismulford <75680004+chrismulford@users.noreply.github.com>
2022-11-18 14:07:05 +0000

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from CollibraObjects.community import Community
-
-
-com = Community('Christopher Mulford')
-print(vars(com))
-
-new_com = Community('CreateTest')
-new_com.create_in_collibra(description='Test Succeeded')
