89bb596b3ab357c61de14857345ade84f2aa180b
chrismulford <chris.mulford@outlook.com>
2022-12-02 10:37:34 +0000

    can now import from objects modue

---
+++ src/CollibraObjects/objects.py
@@ -0,0 +1,7 @@
+from src.CollibraObjects.asset import Asset
+from src.CollibraObjects.assetType import AssetType
+from src.CollibraObjects.attribute import Attribute
+from src.CollibraObjects.attributeType import AttributeType
+from src.CollibraObjects.community import Community
+from src.CollibraObjects.domain import Domain
+from src.CollibraObjects.domainType import DomainType
